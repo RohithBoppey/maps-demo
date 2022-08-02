e3ea5c42197b2c98e8b3ad81d8cd949b6c4e255a
RohithBoppey <rohithboppey1298@gmail.com>
2022-08-02 22:30:29 +0530

    added data points

--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -24,14 +24,93 @@ const markers = [
         markerOffset: '15',
         name: "India",
         coordinates: [78.962883, 20.593683],
-        dataUsage: 300
-    }
+        dataUsage: 700
+    },
+    {
+        markerOffset: '15',
+        name: "India",
+        coordinates: [-122.431297, 37.773972],
+        dataUsage: 575
+    },
+    {
+        markerOffset: '15',
+        name: "Edwardsville",
+        coordinates: [-89.953157, 38.811436],
+        dataUsage: 215
+    },
+    {
+        markerOffset: '15',
+        name: "China",
+        coordinates: [103.0000,  35.0000],
+        dataUsage: 750
+    },
+    {
+        markerOffset: '15',
+        name: "Beijing",
+        coordinates: [116.383331,  39.916668],
+        dataUsage: 412
+    },
+    {
+        markerOffset: '15',
+        name: "Russia",
+        coordinates: [105.318,  61.5240],
+        dataUsage: 780
+    },
+    {
+        markerOffset: '15',
+        name: "Sudan",
+        coordinates: [30.2176,  12.8628],
+        dataUsage: 165
+    },
+    {
+        markerOffset: '15',
+        name: "Egypt",
+        coordinates: [31.233334,  30.033333],
+        dataUsage: 290
+    },
+    {
+        markerOffset: '15',
+        name: "Tanzania",
+        coordinates: [34.8888,  6.3690],
+        dataUsage: 356
+    },
+    {
+        markerOffset: '15',
+        name: "Mali",
+        coordinates: [-3.9861,  17.5739],
+        dataUsage: 415
+    },
+    {
+        markerOffset: '15',
+        name: "Brazil",
+        coordinates: [51.9253,  14.2350],
+        dataUsage: 415
+    },
+    {
+        markerOffset: '15',
+        name: "Mali",
+        coordinates: [-3.9861,  17.5739],
+        dataUsage: 627
+    },
+    {
+        markerOffset: '15',
+        name: "Ukraine",
+        coordinates: [31.1656,  48.3794],
+        dataUsage: 219
+    },
+    {
+        markerOffset: '15',
+        name: "Austraila",
+        coordinates: [133.7751,  25.2744],
+        dataUsage: 250
+    },
+    
 ]
 
 
 const colorScaler = (value) => {
     // Assuming value ranges from 0 to 7500
-    const scaler = chroma.scale(['#03fcdf', '#037bfc']).domain([0, 1000]);
+    const scaler = chroma.scale(['#03fcf8', '#4503fc']).domain([0, 800]);
     return scaler(value);
 }
 
@@ -74,9 +153,9 @@ const SimpleMap = () => {
                 {
                     markers.map(({name, coordinates, offset, dataUsage}) => (
                         <Marker key={name} coordinates={coordinates} onClick= {() => {
-                            setCountryStats(`${dataUsage}TB`);
+                            setCountryStats(`Data Usage: ${dataUsage}TB`);
                         }} >
-                            <circle r={5} fill={`${colorScaler(dataUsage)}`} stroke={`${chroma(colorScaler(dataUsage).darken(1.75))}`}/>
+                            <circle r={dataUsage/100} fill={`${colorScaler(dataUsage)}`} stroke={`${chroma(colorScaler(dataUsage).darken(3))}`}/>
                         </Marker>
                     ))
                 }
